a41c11ad43cacea5f0bcbe88cf7358bb2a15450b
sukyung-kim <63035715+sukyung-kim@users.noreply.github.com>
2020-04-06 17:54:30 +0900

    관리자페이지 운영-판매장의 모바일웹 팝업 상대경로 수정

--- js/shop.js
+++ js/shop.js
@@ -1,7 +1,7 @@
 // 판매장 템플릿
 var tpl_tr_tab1_table = _.template('<tr data-no="<%- no %>"><td><%- no %></td>' +
 	'<td><a href="#" onclick="shop_update(\'<%- no %>\')"><%- name %></a></td><td><%- address1 %></td><td><%- tel %></td>' + 
-	'<td><span class="shopUrl" onclick="window.open(\'https://www.nhhanaromart.com/app/home/main.html?vm_cp_no=<%- no %>\',\'_blank\',\'width=414 , height=650, resizable=no, left=500, top=200\'); localStorage.setItem(\'vm_cp_no\',<%- no %>)">http://www.nhhanaromart.com/app/home/main.html?vm_cp_no=<%- no %></span></td>' +
+	'<td><span class="shopUrl" onclick="window.open(\'../app/home/main.html?vm_cp_no=<%- no %>\',\'_blank\',\'width=414 , height=650, resizable=no, left=500, top=200\'); localStorage.setItem(\'vm_cp_no\',<%- no %>)">http://www.nhhanaromart.com/app/home/main.html?vm_cp_no=<%- no %></span></td>' +
 	'<td><%- regDate %></td>' +
 	//'<% if(delivery === "Y") { %><td class="store_delivery" onclick="delivery()">사용중</td><% } else { %><td>미사용</td><% } %>' +
 	'<% if(sales === "Y") { %><td>영업</td><% } else { %><td>휴업</td><% } %>');
