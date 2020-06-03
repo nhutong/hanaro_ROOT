b2557c85b9582caff21852f9b1d08682e2c5c3a5
sukyung-kim <kacm3838@gmail.com>
2020-06-03 21:10:55 +0900

    최종혜택가 부분 수정

--- app/js/m_leaflet.js
+++ app/js/m_leaflet.js
@@ -378,11 +378,10 @@ function getPdContent(rcv_jd_no) {
 			    }else if (decodeURIComponent(item['coupon_discount']) != ""){
 					var couponed = Number(item['price']) - Number(item['coupon_discount']);
 					text += '    	  <div class="leaflet_modal_price3"><h6 style="font-family: Noto Sans KR; display:inline-block;">쿠폰할인가 '+comma(couponed)+'원</div>'
-				}else if (decodeURIComponent(item['card_discount']) != "" && (item['coupon_discount']) != "");{
+				}else if (decodeURIComponent(item['card_discount']) != "" && decodeURIComponent(item['coupon_discount']) != ""){
 					var summed = Number(item['price']) - Number(item['card_discount'])- Number(item['coupon_discount']);
-					text += '    	  <div class="leaflet_modal_price2"><h6 style="font-family: Noto Sans KR; display:inline-block;">최종혜택가</h6> '+comma(summed)+'원</div>'
+					text += '    	  <div class="leaflet_modal_price2"><h6 style="font-family: Noto Sans KR; display:inline-block;">최종혜택가</h6> '+comma(summed)+'원</div>'	
 				}
-					
 				//200603 김수경 상품상세팝업에 카드할인가와 쿠폰할인가 표시
 
 				text += '    	  <div class="leaflet_txt">'
