09b4a104070fbd299402fe4bbb91e64d8f822a60
sukyung-kim <kacm3838@gmail.com>
2020-06-03 20:08:01 +0900

    상세팝업에 최종혜택가도 표기

--- app/js/m_leaflet.js
+++ app/js/m_leaflet.js
@@ -372,18 +372,19 @@ function getPdContent(rcv_jd_no) {
 				text += '    	  <div class="leaflet_modal_title">'+item['pd_name']+'</div>'
 				text += '    	  <div class="leaflet_modal_price">'+comma(item['price'])+'원</div>'
 				//200603 김수경 상품상세팝업에 카드할인가와 쿠폰할인가 표시
-				if (decodeURIComponent(item['card_discount']) != "") && (decodeURIComponent(item['coupon_discount']) != ""){
+				if (decodeURIComponent(item['card_discount']) != "" && (item['coupon_discount']) != "");{
 					var summed = Number(item['price']) - Number(item['card_discount'])- Number(item['coupon_discount']);
 					text += '    	  <div class="leaflet_modal_price2">최종혜택가 '+comma(summed)+'원</div>'
-				}else if (decodeURIComponent(item['card_discount']) != ""){
+				    }if (decodeURIComponent(item['card_discount']) != ""){
 					var carded = Number(item['price']) - Number(item['card_discount']);
 					text += '    	  <div class="leaflet_modal_price2">카드할인가 '+comma(carded)+'원</div>'
-				}else if (decodeURIComponent(item['coupon_discount']) != ""){
+				    }else if (decodeURIComponent(item['coupon_discount']) != ""){
 					var couponed = Number(item['price']) - Number(item['coupon_discount']);
 					text += '    	  <div class="leaflet_modal_price3">쿠폰할인가 '+comma(couponed)+'원</div>'
-				}
-			
+				}else{
+				}													
 				//200603 김수경 상품상세팝업에 카드할인가와 쿠폰할인가 표시
+
 				text += '    	  <div class="leaflet_txt">'
 				text += '    		  <div class="leaflet_discount">'                 
 				text += '    			 <h5>혜택 및 상품 정보 안내</h5>'
