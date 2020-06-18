b77f95e9c248a4ba202ff4751b8c7c45792c12e5
Kim JB <lapu512@gmail.com>
2020-06-18 15:39:22 +0900

    쿠폰히스토리 화면 오류수정

--- js/coupon/coupon_history.js
+++ js/coupon/coupon_history.js
@@ -83,22 +83,22 @@ $(function () {
 	$("#coupon_start_date").val(year+'-'+month+'-'+sday);
 	$("#coupon_end_date").val(year+'-'+month+'-'+eday);	
 
-	$.datepicker.setDefaults({
-		dateFormat: 'yy-mm-dd',
-		prevText: '이전 달',
-		nextText: '다음 달',
-		monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-		monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
-		dayNames: ['일', '월', '화', '수', '목', '금', '토'],
-		dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
-		dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
-		showMonthAfterYear: true,
-		yearSuffix: '년'
-	});
-	$(function() {
-		$("#coupon_start_date").datepicker();
-		$("#coupon_end_date").datepicker();
-	});	
+	// $.datepicker.setDefaults({
+	// 	dateFormat: 'yy-mm-dd',
+	// 	prevText: '이전 달',
+	// 	nextText: '다음 달',
+	// 	monthNames: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+	// 	monthNamesShort: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+	// 	dayNames: ['일', '월', '화', '수', '목', '금', '토'],
+	// 	dayNamesShort: ['일', '월', '화', '수', '목', '금', '토'],
+	// 	dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'],
+	// 	showMonthAfterYear: true,
+	// 	yearSuffix: '년'
+	// });
+	// $(function() {
+	// 	$("#coupon_start_date").datepicker();
+	// 	$("#coupon_end_date").datepicker();
+	// });	
 
 	// noticeCont(targetCompanyNo, pageNo);
 	// noticeCont_paging(targetCompanyNo, pageNo);
