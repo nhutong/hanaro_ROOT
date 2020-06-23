f964644ca06a0f5d06b1dab9611d7a2c144d50e6
sukyung-kim <kacm3838@gmail.com>
2020-06-23 10:05:14 +0900

    썸머노트 이미지 업로드 테스트

--- js/home.js
+++ js/home.js
@@ -135,8 +135,33 @@ function home_post_create() {
 		maxHeight: null,             // 최대 높이
 		focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
 		lang: "ko-KR",					// 한글 설정
-		placeholder: '최대 2048자까지 쓸 수 있습니다'	//placeholder 설정
-});
+		placeholder: '최대 2048자까지 쓸 수 있습니다',	//placeholder 설정
+		callbacks: { // 콜백을 사용
+			// 이미지를 업로드할 경우 이벤트를 발생
+			onImageUpload: function(files, editor, welEditable) {
+				sendFile(files[0], this);
+			}
+		}
+	});
+	
+  /* summernote에서 이미지 업로드시 실행할 함수 */
+			function sendFile(file, editor) {
+				// 파일 전송을 위한 폼생성
+				 data = new FormData();
+				 data.append("uploadFile", file);
+				 $.ajax({ // ajax를 통해 파일 업로드 처리
+					 data : data,
+					 type : "POST",
+					 url : "./summernote_imageUpload.jsp",
+					 cache : false,
+					 contentType : false,
+					 processData : false,
+					 success : function(data) { // 처리가 성공할 경우
+						// 에디터에 이미지 출력
+						 $(editor).summernote('editor.insertImage', data.url);
+						}
+					});
+				}
   	// 200622 김수경 썸머노트 적용 테스트
 
 // 게시물 등록취소
