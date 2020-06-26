d6f075fece4bf6bc4fdc5b6d4358c301cdf2bb0d
rewg3@naver.com <kacm3838@gmail.com>
2020-06-26 13:45:29 +0900

    add modal

--- js/home.js
+++ js/home.js
@@ -149,6 +149,16 @@ function home_post_create() {
 		focus: true,                  // 에디터 로딩후 포커스를 맞출지 여부
 		lang: "ko-KR",					// 한글 설정
 		placeholder: '최대 2048자까지 쓸 수 있습니다',	//placeholder 설정
+		toolbar: [
+			['style', ['bold', 'italic', 'underline', 'clear']],
+			['fontsize', ['fontsize']],
+			['fontname', ['fontname']],
+			['color', ['color']],
+			['para', ['ul', 'ol', 'paragraph']],
+			['table', ['table']],
+			['insert', ['link', 'picture', 'video']],
+			['view', ['fullscreen', 'codeview', 'help']],
+		  ],			
 		callbacks: { // 콜백을 사용
 			// 이미지를 업로드할 경우 이벤트를 발생
 			onImageUpload: function(files, editor, welEditable) {
