1f0ea60300bcf8c21e07340f21fae18c47b353f1
kimdaeyun <kimdaeyun@hotmail.com>
2020-06-30 16:32:57 +0900

    쿠폰인쇄수정

--- js/coupon/coupon_print.js
+++ js/coupon/coupon_print.js
@@ -16,13 +16,72 @@ $(function(){
 	$("#popPdStartDate").val(year+'-'+month+'-'+day);
 	$("#popPdEndDate").val(year+'-'+month+'-'+day);	
 
+	couponSize('01','A4');
+
 })
 
 
-function couponSize(kk){
-    var c = kk;
+function couponSize(kk, ss){
+
+	//var printSize = $("#printSize").val();
+	//var printOrientation = $('#printOrientation').val();
+	//$("#printSize").val();
+	//$('#printOrientation').val();
+	//var width = $("#width").val();
+	//var height = $("#height").val();
+	//var couponDiv = $("#couponDiv").val();
+		
+	var c = kk;
+	var size = ss;
+
+	$("#couponDiv").val(".coupon"+c);
+
     $(".coupon_print_lft .coupon01,.coupon_print_lft .coupon02,.coupon_print_lft .coupon03").css("display","none");
-    $(".coupon_print_lft .coupon"+c).css("display","block");
+	$(".coupon_print_lft .coupon"+c).css("display","block");
+		
+	$("#printSize").val(size);
+
+	if ( kk == '01' ){ //세로형
+		$('#printOrientation').val('portrait');
+		var a4_width="210mm";
+		var a4_height="297mm";
+		var a3_width="297mm";
+		var a3_height="420mm";			
+	}else if( kk == '02' ){ //가로형
+		$('#printOrientation').val('landscape');
+		var a4_width="297mm";
+		var a4_height="210mm";
+		var a3_width="420mm";
+		var a3_height="297mm";		
+	}else{ //세로형(엔드매대형)
+		$('#printOrientation').val('portrait');
+		var a4_width="210mm";
+		var a4_height="297mm";
+		var a3_width="297mm";
+		var a3_height="420mm";			
+	}
+	
+	if( size == 'A4' ){
+		$("#printSize").val('A4');
+		$("#width").val(a4_width);
+		$("#height").val(a4_height);
+		$(".coupon_print_lft .coupon"+c).css("font-size","14px");				
+	}else{
+		$("#printSize").val('A3');
+		$("#width").val(a3_width);
+		$("#height").val(a3_height);			
+		$(".coupon_print_lft .coupon"+c).css("font-size","20px");				
+	}				
+
+	if ( kk == '03' ){ //세로형	
+	}else{
+		$(".coupon_print_lft .coupon"+c).css("width",$("#width").val());
+		$(".coupon_print_lft .coupon"+c).css("height",$("#height").val());		
+	}
+
+	//console.log($("#printSize").val()+"/"+$('#printOrientation').val());
+
+	// console.log($("#coupon_print_lft").outerWidth()+"/"+$("#coupon_print_lft").outerHeight());
 }
 
 /*쿠폰정보를 가져옴 200110 김나영*/
@@ -57,7 +116,7 @@ function getCouponInfo(coupon_no){
 			if (info.img_path == null){
 			}else{
 				$('#a4_pd_img').append("<img src='/upload/"+info.img_path+"' alt='상품이미지'>");
-				
+				$('#a3_pd_img').append("<img src='/upload/"+info.img_path+"' alt='상품이미지'>");				
 			}
 
 			if(info.coupon_type == 'PRODUCT'){
@@ -67,6 +126,8 @@ function getCouponInfo(coupon_no){
 				$('#price_area').show();
 				$('#product_area').hide();
 			}
+
+			couponPrintApply();
 			
 		});
 	}
@@ -95,6 +156,7 @@ $("#couponPrintSave").on("click",function(e){
 	function(resultJSON){
 		if(resultJSON['update'] > 0){
 			alert('저장이 완료되었습니다');
+			couponPrintApply();			
 		}else {
 			console.log(resultJSON['error']);
 			alert("저장 중 에러가 발생하였습니다.");
@@ -117,6 +179,10 @@ $("#popPdEndDate").on("change",function(e){
 //적용하기
 $("#couponPrintApply").on("click",function(e){
 	e.preventDefault();
+	couponPrintApply();    
+});
+
+function couponPrintApply(){
 
 	/* A4 세로형 */
 
@@ -233,20 +299,25 @@ $("#couponPrintApply").on("click",function(e){
 	$("#r200Discount").empty();
 	$("#r200Discount").append(comma($("#popPdDiscount").val())+"원");
 	
-
-	
-    if( $("#popPdInfo").val() == ""){
+    if( $("#popPdInfo").val() == "" || $("#popPdInfo").val() == undefined ){
 	}else{
 		$(".pop_pd_info").empty();
 		$(".pop_pd_info").append($("#popPdInfo").val());
 	}
-    
-});
+}
 
 $("#couponPrintPrint").on("click",function(e){
 	e.preventDefault();
-	var width = $("#coupon_print_lft").outerWidth();
-	var height = $("#coupon_print_lft").outerHeight();
+	//var width = $("#coupon_print_lft").outerWidth();
+	//var height = $("#coupon_print_lft").outerHeight();
+
+	var width = $( $("#couponDiv").val() ).outerWidth();
+	var height = $( $("#couponDiv").val() ).outerHeight();
+
+	//var width = $("#width").val();
+	//var height = $("#height").val();	
+	var printSize = $("#printSize").val();
+	var printOrientation = $('#printOrientation').val();
 
 	// alert($("#coupon_print_lft").outerHeight());
 	// alert($("#coupon_print_lft").outerWidth());
@@ -254,28 +325,56 @@ $("#couponPrintPrint").on("click",function(e){
 
 	/** 프린트 버튼 클릭 시 이벤트 */
     var $container = $("#coupon_print_lft").clone();  // 프린트 할 특정 영역 복사
-	// var cssText = "";                                 // 스타일 복사
 	// console.log("aaaa");
 	// console.log($("style"));
-    // for ( var node of $("style") ) {
-    //     cssText += node.innerHTML;
-    // }
+
+	var cssText = "";
+
+    for ( var node in $("style") ) {
+		console.log(node);
+        cssText += node.innerHTML;
+	}
+	
     /** 팝업 */
     var innerHtml = $container[0].innerHTML;
 	var popupWindow = window.open("", "_blank", "width="+width+"px,height="+height+"px");
 	//var popupWindow = window.open("", "_blank", "width=500px,height=600px");
-    popupWindow.document.write("<!DOCTYPE html>"+
-	  "<html>"+
-	  	"<link href='../css/common.css'        rel='stylesheet'>"+
-	  	"<link href='../css/index.css'         rel='stylesheet'>"+
-	  	// "<link href='../css/page_print.css'    rel='stylesheet'>"+
-	  	"<link href='../css/bootstrap.min.css' rel='stylesheet'>"+
-	  	"<link href='../css/all.css'           rel='stylesheet'>"+
-	  	"<head>"+
-        // "<style>"+cssText+"</style>"+
-        "</head>"+
-        "<body>"+innerHtml+"</body>"+
-      "</html>");
+
+	//$('head').append('<style>@page{size: landscape;}</style>')
+	// cssText = "@page{size: landscape;}";
+	// cssText = "@media print{@page {size: landscape}}";
+	// cssText = "@media print { html, body { width: 297mm; height: 210mm; } }";
+
+    //size: A3;
+    //size: landscape;
+	//margin: 0;    	
+	var text = "";
+
+	text += "<!DOCTYPE html>"+
+		"<html>"+
+			"<link href='../css/common.css'        rel='stylesheet'>"+
+			"<link href='../css/index.css'         rel='stylesheet'>"+
+			"<link href='../css/page_print.css'    rel='stylesheet'>"+
+			"<link href='../css/bootstrap.min.css' rel='stylesheet'>"+
+			"<link href='../css/all.css'           rel='stylesheet'>"+
+			"<head>"+
+			//"<style>"+cssText+"</style>"+
+			"</head>"+
+			"<body>";
+
+	if ( $("#couponDiv").val() == '.coupon03' ){
+		text += "<div>"+innerHtml+"</div>"+
+				"<div>"+innerHtml+"</div>"+
+				"<div>"+innerHtml+"</div>"+
+				"<div>"+innerHtml+"</div>"+								
+			    "<div>"+innerHtml+"</div>";
+	}else{
+		text += "<div>"+innerHtml+"</div>";
+	}
+
+	text += "</body></html>";
+
+    popupWindow.document.write(text);
 
     popupWindow.document.close();
     popupWindow.focus();
