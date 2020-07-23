2e49a38bc18a3189dcd77d91f1d482485aa6f853
ByeongJooKwon <rewg3@naver.com>
2020-07-23 12:23:52 +0900

    popup show value change css add

--- js/popup_list.js
+++ js/popup_list.js
@@ -76,12 +76,36 @@ var tpl_tr_tab1_table = _.template('<tr id="member<%- popup_no %>" data-no="<%- 
 	'<input type="radio" id="show_flagN" name="show_flag<%-flagNo%>" <%- showFlagN %> onclick="javascript:changeShowFlag(\'<%-popup_no%>\', \'N\')"> 미반영</td>'
 );
 
+var tpl_tr_tab1_table_2 = _.template('<tr id="member<%- popup_no %>" data-no="<%- popup_no %>"><td><%- popup_no %></td>' +
+	'<td><a href="popup_edit.html?popupNo=<%- popup_no %>" ><%- popup_title %></a></td><td><a href="popup_edit.html?popupNo=<%- popup_no %>" ><img src="<%- img_url %>"/></a></td><td><%- company_name %></td><td><%- period %></td>' + 	
+	'<td><%- reg_name %></td><td><%- lst_name %></td><td><%- reg_date %></td><td>' +
+	'<li class="tg-list-item" style="list-style: none;">' +
+	'<input class="tgl tgl-ios" id="cb<%- popup_no %>" type="checkbox" <%- showFlagY %> onclick="javascript:changeShowFlag2(\'<%-popup_no%>\')">' +
+	'<label class="tgl-btn" for="cb<%- popup_no %>"></label>' +
+	'</li>' +
+	'</td>'
+);
+
+function changeShowFlag2(no) {
+	let flag;
+	if ($("#cb"+no).prop("checked")) {
+		flag = "Y";
+	} else {
+		flag = "N";
+	}
+	$.ajax({
+        url:'/back/04_home/changeShowFlag.jsp?random=' + (Math.random()*99999), 
+        data : {popNo: no, show_flag: flag},
+        method : 'POST' 
+	});
+}
+
 function changeShowFlag(no, flag) {
 	$.ajax({
         url:'/back/04_home/changeShowFlag.jsp?random=' + (Math.random()*99999), 
         data : {popNo: no, show_flag: flag},
         method : 'POST' 
-    });
+	});
 }
 
 $(function () {
@@ -134,7 +158,7 @@ function getPopupList(compNo){
 					item['reg_date'] 	= datey.getFullYear() + "-";
 					item['reg_date'] += ( datey.getMonth() + 1 ).toString().length > 1 ? datey.getMonth() + 1 : "0" + (datey.getMonth() + 1);
 					item['reg_date'] += "-" + ((datey.getDate().toString().length > 1) ? datey.getDate() : "0" + datey.getDate());
-					$tbody.append(tpl_tr_tab1_table(item));
+					$tbody.append(tpl_tr_tab1_table_2(item));
 				}
 			);
 		},
