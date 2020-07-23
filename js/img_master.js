eb979817628faf1d75d2747bc5722770bcb5e531
ByeongJooKwon <rewg3@naver.com>
2020-07-23 10:59:17 +0900

    es6 change

--- js/img_master.js
+++ js/img_master.js
@@ -29,7 +29,7 @@ enterUpload2.addEventListener('click', function(evt){
 	document.getElementById("spinnerAction").style.display = "block";
 	document.scrollingElement.scrollTop = 0;
 	document.body.style.overflow = "hidden";
-	promiseUploadFile("").then(res => {
+	promiseUploadFile("").then(function(res) {
 		if(res == "cancelButton") {
 			// location.reload(true);
 			document.getElementById("spinnerAction").style.display = "none";
